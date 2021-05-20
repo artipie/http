9844bf003c60cf42d14076dc61c40a5949e8f5e7
Kirill <g4s8.public@gmail.com>
2021-05-20 17:08:02 +0300

    ByteBuffer chunks tokenizer with a test
    For #299 - added tokenizer for `ByteBuffer` chunks processing to split
    by delimiter into series of chunks sequences. Added tests with different
    scenarios. Moved all multipart related classes to a new package. Removed
    unfinished implementation with similar purpose, due to incorrect design.

--- src/main/java/com/artipie/http/rq/multipart/ByteBufferTokenizer.java
+++ src/main/java/com/artipie/http/rq/multipart/ByteBufferTokenizer.java
@@ -1,31 +1,42 @@
-package com.artipie.http.rq;
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/npm-adapter/LICENSE.txt
+ */
+package com.artipie.http.rq.multipart;
 
 import java.io.Closeable;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 /**
- * TODO: description
+ * ByteBuffer tokenizer splits sequence of chunks of {@link ByteBuffer}s separated
+ * by the delimiter bytes into token series of sequences of ByteBuffers,
+ * and notify the receiver at the end of each token with boolean flag.
+ * <p>
+ * This tokenizer updated with {@code push} method, which appends the chunk
+ * buffer into the buffer. The receiver should be specified in the constructor
+ * and handles on next token chunk with boolean flag.
+ * <br/>
+ * Tokenizer can keep the intermediate buffer after push call if it can't determine
+ * the end of the token. The buffer size is always fewer than delimiter length.
+ * <br/>
+ * When chunk stream is ended, tokenizer should be notified with {@code close}
+ * method, it will flush the buffer to receiver if any temporary state exist.
+ * </p>
  *
- * @implNote This class is not thread safe, the access to push and flush should be synchronized externally if needed
- * @implNote The state could be broken in case if runtime exception occurs during the tokenization process,
- * it couldn't be recovered and should not be used after failure.
+ * @implNote This class is not thread safe, the access to push and close
+ *  should be synchronized externally if needed
+ * @implNote The state could be broken in case of runtime exception occurs during
+ *  the tokenization process, it couldn't be recovered and should not be used after failure
+ * @since 1.0
+ * @checkstyle MethodBodyCommentsCheck (500 lines)
  */
 public final class ByteBufferTokenizer implements Closeable {
 
+    /**
+     * Empty read-only buffer.
+     */
     private static final ByteBuffer EMPTY_BUF = ByteBuffer.allocate(0).asReadOnlyBuffer();
-
-    @FunctionalInterface
-    public interface Receiver {
-
-        /**
-         * Receive next chunk of current token.
-         *
-         * @param next Chunk
-         * @param end  True if the end of current token
-         */
-        void receive(ByteBuffer next, boolean end);
-    }
 
     /**
      * Default capacity is 4 Kb.
@@ -38,7 +49,7 @@ public final class ByteBufferTokenizer implements Closeable {
     private final byte[] delim;
 
     /**
-     * Token receiver.
+     * Tokens receiver.
      */
     private final Receiver receiver;
 
@@ -47,10 +58,23 @@ public final class ByteBufferTokenizer implements Closeable {
      */
     private ByteBuffer buffer;
 
+    /**
+     * New tokenizer.
+     *
+     * @param receiver Tokens receiver
+     * @param delim Delimiter
+     */
     public ByteBufferTokenizer(final Receiver receiver, final byte[] delim) {
         this(receiver, delim, ByteBufferTokenizer.CAP_DEFAULT);
     }
 
+    /**
+     * New tokenizer with specified initial capacity of buffer.
+     *
+     * @param receiver Tokens receiver
+     * @param delim Delimiter
+     * @param cap Initial capacity
+     */
     public ByteBufferTokenizer(final Receiver receiver, final byte[] delim, final int cap) {
         this.receiver = receiver;
         this.delim = Arrays.copyOf(delim, delim.length);
@@ -60,58 +84,57 @@ public final class ByteBufferTokenizer implements Closeable {
     /**
      * Push next chunk of data to tokenizer.
      *
-     * @param chunk Data
+     * @param chunk Next chunk
      */
+    @SuppressWarnings("PMD.AssignmentInOperand")
     public void push(final ByteBuffer chunk) {
         this.check();
         this.append(chunk);
-//        final ByteBuffer dup = this.buffer.duplicate();
-//        dup.flip();
         final byte[] arr = ByteBufferTokenizer.array(this.buffer);
         // bid is a next boundary id, offset is current offset of token + boundary
-        int bid, offset = 0;
+        int bid;
+        int offset = 0;
         // find next boundary token with offset as `bid`
-        while ((bid = indexOf(offset, arr, delim)) >= 0) {
+        while ((bid = indexOf(offset, arr, this.delim)) >= 0) {
             if (bid == 0) {
                 // if boundary seq is a head, then next token is and empty token
-                this.receiver.receive(EMPTY_BUF, true);
+                this.receiver.receive(ByteBufferTokenizer.EMPTY_BUF, true);
                 // and offset for next lookup is a delimiter length
-                offset = delim.length;
+                offset = this.delim.length;
                 continue;
             }
             // set bounds for next tokens from current offset to next delimiter id
             this.receiver.receive(copy(this.buffer, offset, bid), true);
             // update offset to token position + delimiter length
-            offset = bid + delim.length;
+            offset = bid + this.delim.length;
         }
         // if next delimiter was not found, then try to send save range of bytes to receiver,
         // since it may contain next delimiter partially, then safe range is a:
         // (buffer-length - (delimiter - 1))
         final int margin = arr.length - this.delim.length + 1;
-        if (margin <= 0) {
-            // skip small buffers
-            return;
+        if (margin > 0) {
+            if (offset < margin) {
+                // if there are some bytes between last offset and margin, then send it
+                // to the receiver and update buffer bounds
+                this.receiver.receive(copy(this.buffer, offset, margin), false);
+                this.buffer.position(margin);
+            } else {
+                // if the offset is crossing possible delimiter margin,
+                // then just reset to the offset position
+                this.buffer.position(offset);
+            }
+            // remove processed bytes and reset buffer limits
+            this.buffer.compact();
+            this.buffer.limit(this.buffer.position());
         }
-        if (offset < margin) {
-            // if there are some bytes between last offset and margin, then send it
-            // to the receiver and update buffer bounds
-            this.receiver.receive(copy(this.buffer, offset, margin), false);
-            this.buffer.position(margin);
-        } else {
-            // if the offset is crossing possible delimiter margin, then just reset to the offset position
-            this.buffer.position(offset);
-        }
-        // remove processed bytes and reset buffer limits
-        this.buffer.compact();
-        this.buffer.limit(this.buffer.position());
     }
 
-    //TODO: remove after debugging
-    static String __bufDebug(final ByteBuffer buffer) {
-        final ByteBuffer dup = buffer.duplicate();
-        final byte[] arr = new byte[dup.remaining()];
-        dup.get(arr);
-        return new String(arr);
+    @Override
+    @SuppressWarnings("PMD.NullAssignment")
+    public void close() {
+        this.flush();
+        // assign to null means broken state, it's verified by `check` method.
+        this.buffer = null;
     }
 
     /**
@@ -120,12 +143,12 @@ public final class ByteBufferTokenizer implements Closeable {
      * @param chunk To append
      */
     private void append(final ByteBuffer chunk) {
-        System.out.printf("append %s (%s) to %s (%s): ", chunk, __bufDebug(chunk), this.buffer, __bufDebug(buffer));
         if (this.buffer.capacity() - this.buffer.limit() >= chunk.remaining()) {
             this.buffer.limit(this.buffer.limit() + chunk.remaining());
             this.buffer.put(chunk);
         } else {
-            final ByteBuffer resized = ByteBuffer.allocate(this.buffer.capacity() + chunk.capacity());
+            final ByteBuffer resized =
+                ByteBuffer.allocate(this.buffer.capacity() + chunk.capacity());
             final int pos = this.buffer.position();
             final int lim = this.buffer.limit();
             this.buffer.flip();
@@ -135,7 +158,6 @@ public final class ByteBufferTokenizer implements Closeable {
             resized.put(chunk);
             this.buffer = resized;
         }
-        System.out.printf("%s (%s)\n", this.buffer, __bufDebug(buffer));
     }
 
     /**
@@ -145,16 +167,12 @@ public final class ByteBufferTokenizer implements Closeable {
         this.check();
         this.buffer.flip();
         if (this.buffer.hasRemaining()) {
-            this.receiver.receive(copy(this.buffer, this.buffer.position(), this.buffer.limit()), true);
+            this.receiver.receive(
+                copy(this.buffer, this.buffer.position(), this.buffer.limit()), true
+            );
         } else {
-            this.receiver.receive(EMPTY_BUF, true);
+            this.receiver.receive(ByteBufferTokenizer.EMPTY_BUF, true);
         }
-    }
-
-    @Override
-    public void close() {
-        this.flush();
-        this.buffer = null;
     }
 
     /**
@@ -168,12 +186,14 @@ public final class ByteBufferTokenizer implements Closeable {
      * Finds index of token in array starting with offset.
      *
      * @param offset Offset to start
-     * @param array  Source array
-     * @param token  Token to find
+     * @param array Source array
+     * @param token Token to find
      * @return Position of token or -1 if not found
      */
-    public static int indexOf(final int offset, final byte[] array, final byte[] token) {
-        for (int i = offset; i < array.length - token.length + 1; ++i) {
+    private static int indexOf(final int offset, final byte[] array, final byte[] token) {
+        int res = -1;
+        // @checkstyle LocalVariableNameCheck (10 lines)
+        TOP: for (int i = offset; i < array.length - token.length + 1; ++i) {
             boolean found = true;
             for (int j = 0; j < token.length; ++j) {
                 if (array[i + j] != token[j]) {
@@ -182,12 +202,19 @@ public final class ByteBufferTokenizer implements Closeable {
                 }
             }
             if (found) {
-                return i;
+                res = i;
+                break TOP;
             }
         }
-        return -1;
+        return res;
     }
 
+    /**
+     * Byte array from byte buffer.
+     *
+     * @param buffer Data holder
+     * @return Byte array
+     */
     private static byte[] array(final ByteBuffer buffer) {
         final ByteBuffer dup = buffer.duplicate();
         dup.flip();
@@ -225,5 +252,22 @@ public final class ByteBufferTokenizer implements Closeable {
         final ByteBuffer buf = ByteBuffer.allocate(cap);
         buf.flip();
         return buf;
+    }
+
+    /**
+     * Tokenizer series receiver.
+     *
+     * @since 1.0
+     */
+    @FunctionalInterface
+    public interface Receiver {
+
+        /**
+         * Receive next chunk of current token.
+         *
+         * @param next Chunk of current token
+         * @param end True if the end of current token
+         */
+        void receive(ByteBuffer next, boolean end);
     }
 }
